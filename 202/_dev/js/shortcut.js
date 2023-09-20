995e72723039baf43df3188547f97bb67b6bdecd
kgleizes <kgleizes@202-ecommerce.com>
2023-09-20 13:00:21 +0200

    refs #41031 - add ECS mention on all shortcuts

--- 202/_dev/js/shortcut.js
+++ 202/_dev/js/shortcut.js
@@ -151,13 +151,13 @@ const Shortcut = {
     url.searchParams.append('action', 'CreateOrder');
     this.updateInfo();
     data['page'] = this.page;
+    data['sc'] = true;
 
 
     if (this.page == 'product') {
       data['idProduct'] = this.idProduct;
       data['quantity'] = this.productQuantity;
       data['combination'] = this.combination.join('|');
-      data['sc'] = true;
     }
 
     if (this.isAddAddress) {
@@ -186,12 +186,12 @@ const Shortcut = {
     url.searchParams.append('action', 'CheckAvailability');
     this.updateInfo();
     data['page'] = this.page;
+    data['sc'] = true;
 
     if (this.page == 'product') {
       data['idProduct'] = this.idProduct;
       data['quantity'] = this.productQuantity;
       data['combination'] = this.combination.join('|');
-      data['sc'] = true;
     }
 
     fetch(url.toString(),
